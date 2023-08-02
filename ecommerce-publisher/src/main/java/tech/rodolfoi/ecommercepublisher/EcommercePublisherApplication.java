e4a931d10d2f1193d3a8a29e44a031ff9c229fdb
Rodolfo Infantini <rodolfo.infantini@gmail.com>
2023-08-02 19:32:38 -0300

    feat(publisher): creating project

---
+++ ecommerce-publisher/src/main/java/tech/rodolfoi/ecommercepublisher/EcommercePublisherApplication.java
@@ -0,0 +1,13 @@
+package tech.rodolfoi.ecommercepublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommercePublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommercePublisherApplication.class, args);
+	}
+
+}
---
+++ ecommerce-publisher/src/test/java/tech/rodolfoi/ecommercepublisher/EcommercePublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.rodolfoi.ecommercepublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercePublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
