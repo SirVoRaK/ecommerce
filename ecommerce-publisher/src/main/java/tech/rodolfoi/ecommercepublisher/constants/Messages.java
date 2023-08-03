733ad40e2d003699f59227982b9f93392188ce08
Rodolfo Infantini <rodolfo.infantini@gmail.com>
2023-08-02 22:18:38 -0300

    feat(publisher): creating messages constants

---
+++ ecommerce-publisher/src/main/java/tech/rodolfoi/ecommercepublisher/constants/Messages.java
@@ -0,0 +1,6 @@
+package tech.rodolfoi.ecommercepublisher.constants;
+
+public abstract class Messages {
+    public static final String ORDER_CREATED = "Order created successfully";
+    public static final String ORDER_CREATION_FAILED = "Order creation failed";
+}
