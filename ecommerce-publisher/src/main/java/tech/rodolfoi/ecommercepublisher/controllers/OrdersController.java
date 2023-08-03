65663ee39ab942b6d3fb2cb767deeacecf2dfbe2
Rodolfo Infantini <rodolfo.infantini@gmail.com>
2023-08-02 22:19:23 -0300

    feat(publisher): creating the controller for the graphql endpoint

---
+++ ecommerce-publisher/src/main/java/tech/rodolfoi/ecommercepublisher/controllers/OrdersController.java
@@ -0,0 +1,36 @@
+package tech.rodolfoi.ecommercepublisher.controllers;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import tech.rodolfoi.ecommercepublisher.constants.Messages;
+import tech.rodolfoi.ecommercepublisher.dtos.Order;
+import tech.rodolfoi.ecommercepublisher.dtos.ResultResponse;
+import tech.rodolfoi.ecommercepublisher.dtos.inputs.OrderInput;
+import tech.rodolfoi.ecommercepublisher.services.interfaces.OrderService;
+
+@Controller
+public class OrdersController {
+    @Autowired
+    private OrderService orderService;
+
+    @QueryMapping
+    public Iterable<Order> orders() {
+        return new ArrayList<>();
+    }
+
+    @MutationMapping
+    public ResultResponse createOrder(@Argument OrderInput order) {
+        try {
+            orderService.create(order);
+            return new ResultResponse(true, Messages.ORDER_CREATED);
+        } catch (Exception e) {
+            return new ResultResponse(false, Messages.ORDER_CREATION_FAILED);
+        }
+    }
+}
