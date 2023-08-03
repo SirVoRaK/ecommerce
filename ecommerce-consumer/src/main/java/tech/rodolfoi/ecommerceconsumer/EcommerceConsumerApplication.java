7f0261e68900965c69c67962ab1b069d420e344b
Rodolfo Infantini <rodolfo.infantini@gmail.com>
2023-08-03 12:16:07 -0300

    feat(consumer): base spring project

---
+++ ecommerce-consumer/src/main/java/tech/rodolfoi/ecommerceconsumer/EcommerceConsumerApplication.java
@@ -0,0 +1,13 @@
+package tech.rodolfoi.ecommerceconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceConsumerApplication.class, args);
+	}
+
+}
